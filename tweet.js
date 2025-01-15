0760b22f14fa8b9776ecf33b29b5fe9d9b02d9f5
kafessizaksiyon <aksiyon2@kafessizturkiye.com>
2025-01-15 20:17:25 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,3 +1,3 @@
-const address = "mailto:iletisim@d-ream.com.tr?bcc=aksiyon2@kafessizturkiye.com"
+const address = "mailto:diletisim@dogusgrubu.com.tr,iletisim@d-ream.com.tr,info@dhotelier.com.tr?bcc=aksiyon2@kafessizturkiye.com"
 let link = address
 window.location.replace(link);
