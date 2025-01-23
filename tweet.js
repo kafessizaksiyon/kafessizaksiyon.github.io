63117866593239be9308148e9373b879e98b40fd
kafessizaksiyon <aksiyon2@kafessizturkiye.com>
2025-01-23 17:45:54 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,3 +1,13 @@
-const address = "mailto:diletisim@dogusgrubu.com.tr,iletisim@d-ream.com.tr,info@dhotelier.com.tr?bcc=aksiyon2@kafessizturkiye.com"
-let link = address
-window.location.replace(link);
+const addresses = [
+    "https://maps.app.goo.gl/ACwogkGZdT3qr76d6",
+    "https://maps.app.goo.gl/G5JS4siJidqmooaR8",
+    "https://maps.app.goo.gl/iV4b6FskT4LZGWKj7"
+    
+
+    
+
+];
+
+const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
+
+window.location.replace(address_1);
